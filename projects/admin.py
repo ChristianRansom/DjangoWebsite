eb5ec16fdc0e7710c48fd2d8a0bf57ba0281a42c
Christian Ransom <christian.ransom123@gmail.com>
2021-10-07 13:25:59 -0700

    Added projects to site administration

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
