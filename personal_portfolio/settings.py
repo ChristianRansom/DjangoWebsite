35e844839b4b50cffd2eafd8b6fcb8eeb9326540
Christian Ransom <christian.ransom123@gmail.com>
2021-10-08 08:14:03 -0700

    Added real SMTP server to send emails

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -133,6 +133,8 @@ LOGOUT_REDIRECT_URL = "dashboard"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-EMAIL_HOST = "localhost"
-EMAIL_PORT = 1025
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
