17dcf74adc53037f388e061b147c7e9e28a9cc60
Christian Ransom <christian.ransom123@gmail.com>
2021-10-24 07:55:29 -0700

    Fixed static files again

--- personal_portfolio/settings/default.py
+++ personal_portfolio/settings/default.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+print("static_root: " + str(STATIC_ROOT))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'personal_portfolio/static'),
+)
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "dashboard"
 
