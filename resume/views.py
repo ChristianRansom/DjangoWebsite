3901b95282e0ddce07910e506bb5511519bfd8e3
Christian Ransom <christian.ransom123@gmail.com>
2021-10-28 23:47:25 -0700

    Simplified and improved project cards look

--- resume/views.py
+++ resume/views.py
@@ -42,7 +42,7 @@ display_size = 30
 skills = []
 skills.append("mysql > SELECT Software FROM skills  WHERE Category ='Software' ORDERBY 'mastery';")
 skills.append("+" + "-"*display_size + "+" + "-"*display_size + "+")
-skills.append("|" + str("skill").center(display_size) + "|" + str("mastery").center(display_size) + "|")
+skills.append("|" + str("skill").center(display_size) + "|" + str("proficiency").center(display_size) + "|")
 skills.append("+" + "-"*display_size + "+" + "-"*display_size + "+")
 
 for skill, mastery in skill_list.items():
