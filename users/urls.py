c0cf03bec0874608e1691d77f24c645406c9c632
Christian Ransom <christian.ransom123@gmail.com>
2021-11-20 19:36:30 -0800

    Added option to sign in with google account

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from users.views import dashboard, register, user_settings
+from . import views
+
 
 urlpatterns = [
     url(r"^accounts/", include("django.contrib.auth.urls")),
@@ -7,5 +9,6 @@ urlpatterns = [
     url(r"^register/", register, name="register"),
     url(r"^oauth/", include("social_django.urls")),
     url(r"^user_settings/", user_settings, name="user_settings"),
+    url(r'^google-login/$', views.google_login, name="google_login"),
     url('', include('social_django.urls', namespace='social'))
 ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,9 @@ from django.urls import reverse
 from users.forms import CustomUserCreationForm
 from resume.resume_data import *
 from blog.models import Post
-from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def dashboard(request):
@@ -17,6 +19,7 @@ def dashboard(request):
         "posts": posts,
     }
     return render(request, "users/dashboard.html", context)
+
 
 def register(request):
     if request.method == 'POST':
@@ -32,3 +35,60 @@ def register(request):
 
 def user_settings(request):
     return render(request, 'users/user_settings.html', {})
+
+
+def google_login(request):
+    redirect_uri = "%s://%s%s" % (
+        request.scheme, request.get_host(), reverse('dashboard')
+    )
+    print(redirect_uri)
+    if('code' in request.GET):
+        params = {
+            'grant_type': 'authorization_code',
+            'code': request.GET.get('code'),
+            'redirect_uri': redirect_uri,
+            'client_id': settings.GP_CLIENT_ID,
+            'client_secret': settings.GP_CLIENT_SECRET
+        }
+        url = 'https://accounts.google.com/o/oauth2/token'
+        response = request.post(url, data=params)
+        url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+        access_token = response.json().get('access_token')
+        response = request.get(url, params={'access_token': access_token})
+        user_data = response.json()
+        email = user_data.get('email')
+        if email:
+            user, _ = User.objects.get_or_create(email=email, username=email)
+            gender = user_data.get('gender', '').lower()
+            if gender == 'male':
+                gender = 'M'
+            elif gender == 'female':
+                gender = 'F'
+            else:
+                gender = 'O'
+            data = {
+                'first_name': user_data.get('name', '').split()[0],
+                'last_name': user_data.get('family_name'),
+                'google_avatar': user_data.get('picture'),
+                'gender': gender,
+                'is_active': True
+            }
+            user.__dict__.update(data)
+            user.save()
+            user.backend = settings.AUTHENTICATION_BACKENDS[0]
+            login(request, user)
+        else:
+            messages.error(
+                request,
+                'Unable to login with Gmail Please try again'
+            )
+        return redirect('/')
+    else:
+        url = "https://accounts.google.com/o/oauth2/auth?client_id=%s&response_type=code&scope=%s&redirect_uri=%s&state=google"
+        scope = [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email"
+        ]
+        scope = " ".join(scope)
+        url = url % (settings.GP_CLIENT_ID, scope, redirect_uri)
+        return redirect(url)
