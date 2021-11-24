e73f5e6f432b9a6808b3c668b0d7f64d131103e1
Christian Ransom <christian.ransom123@gmail.com>
2021-11-23 16:28:03 -0800

    Add Resume download

--- resume/urls.py
+++ resume/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.resume_index, name="resume_index"),
     path("project/<int:pk>/", views.project_detail, name="project_detail"),
+    path("download/<str:path>/", views.download, name="download"),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,7 @@
-import random
-import string
 from resume.resume_data import *
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
 
 from django.shortcuts import render
 
@@ -20,3 +21,13 @@ def project_detail(request, pk):
         'project': projects[pk]
     }
     return render(request, 'project_detail.html', context)
+
+
+def download(request, path):
+    file_path = os.path.join(settings.STATIC_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
